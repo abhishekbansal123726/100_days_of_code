74274cb174692135ec2ade6329cc5f31c0c0fa58
abhishekbansal123726 <abhishekbansal2806@gmail.com>
2025-09-11 02:35:38 +0530

    Update que11.c

--- Day06/que11.c
+++ Day06/que11.c
@@ -1,1 +1,37 @@
+Q11: Write a program to input an integer and check whether it is even or odd using if–else.
+
+/*
+Sample Test Cases:
+Input 1:
+7
+Output 1:
+7 is odd
+
+Input 2:
+12
+Output 2:
+12 is even
+
+*/
+
+//Solution ; An integer is even if it is divisible by 2 (num % 2 == 0).Otherwise, it is odd.If else function is also used
+
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    // Input an integer
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    // Check even or odd using if–else
+    if (num % 2 == 0) {
+        printf("%d is even\n", num);
+    } else {
+        printf("%d is odd\n", num);
+    }
+
+    return 0;
+}
 
