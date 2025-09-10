a152dacd6c489453bef23ef0b298da6118831d21
abhishekbansal123726 <abhishekbansal2806@gmail.com>
2025-09-11 02:31:52 +0530

    Update que5.c

--- Day 03/que5.c
+++ Day 03/que5.c
@@ -1,1 +1,36 @@
 
+Q5: Write a program to convert temperature from Celsius to Fahrenheit.
+
+/*
+Sample Test Cases:
+Input 1:
+0
+Output 1:
+Fahrenheit=32
+
+Input 2:
+100
+Output 2:
+Fahrenheit=212
+
+*/
+
+//Solution : in this float function and the formula F= (9/5 * C)  + 32  is used to convert from celsius to farenheit
+
+#include <stdio.h>
+
+int main() {
+    float celsius, fahrenheit;
+
+    // Input temperature in Celsius
+    printf("Enter temperature in Celsius: ");
+    scanf("%f", &celsius);
+
+    // Convert to Fahrenheit
+    fahrenheit = (celsius * 9 / 5) + 32;
+
+    // Printing the output
+    printf("Fahrenheit=%.0f\n", fahrenheit);
+
+    return 0;
+}
