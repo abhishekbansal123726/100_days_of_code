4f9cc236f5306f53d97237c0dc0dbc77baf215c8
abhishekbansal123726 <abhishekbansal2806@gmail.com>
2025-09-11 02:35:04 +0530

    Update que10.c

--- Day 05/que10.c
+++ Day 05/que10.c
@@ -1,4 +1,4 @@
-Q9: Write a program to calculate simple and compound interest for given principal, rate, and time.
+Q10: Write a program to calculate simple and compound interest for given principal, rate, and time.
 
 /*
 Sample Test Cases:
