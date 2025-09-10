f9ebac7994eb74dcb09a562eef74091dcad230c8
abhishekbansal123726 <abhishekbansal2806@gmail.com>
2025-09-11 02:32:10 +0530

    Update que6.c

--- Day 03/que6.c
+++ Day 03/que6.c
@@ -1,1 +1,37 @@
+Q6: Write a program to swap two numbers using a third variable.
 
+/*
+Sample Test Cases:
+Input 1:
+3 5
+Output 1:
+After swap: 5 3
+
+Input 2:
+-1 1
+Output 2:
+After swap: 1 -1
+
+*/
+
+//Solution : For solving this we need to have 3 varibales out of which 2 will be constant and 1 will be temporary 
+
+#include <stdio.h>
+
+int main() {
+    int a, b, akshit;
+
+    // Taking input two numbers
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    // Swaping using variable "akshit"
+    akshit = a;
+    a = b;
+    b = akshit;
+
+    // Printing output
+    printf("After swap: %d %d\n", a, b);
+
+    return 0;
+}
