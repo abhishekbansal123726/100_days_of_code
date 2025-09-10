5edd03d7db45a9f47c3110a56af0fa5f2488cadb
abhishekbansal123726 <abhishekbansal2806@gmail.com>
2025-09-11 02:37:01 +0530

    Update que13.c

--- Day07/que13.c
+++ Day07/que13.c
@@ -1,1 +1,39 @@
+Q13: Write a program to input a year and check whether it is a leap year or not using conditional statements.
 
+/*
+Sample Test Cases:
+Input 1:
+2020
+Output 1:
+Leap year
+
+Input 2:
+1900
+Output 2:
+Not a leap year
+
+Input 3:
+2000
+Output 3:
+Leap year
+
+*/
+
+//Solution : for this      1. Divisible by 400 -> Leap year , 2. Divisible by 4 but not by 100 -> Leap year
+
+#include <stdio.h>
+
+int main() {
+    int year;
+    printf("Enter a year: ");
+    scanf("%d", &year);
+
+    // Leap year conditions:
+    if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+        printf("Leap year\n");
+    } else {
+        printf("Not a leap year\n");
+    }
+
+    return 0;
+}
