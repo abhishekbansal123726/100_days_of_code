be45e1cd8e7216635208197292a835fccc14d71b
abhishekbansal123726 <abhishekbansal2806@gmail.com>
2025-09-13 14:06:28 +0530

    Update que35.c

--- Day18/que35.c
+++ Day18/que35.c
@@ -1,1 +1,40 @@
 
+This is my day 18 progress for 100 days of code challenge 
+
+Q35: Write a program to print all factors of a given number.
+
+/*
+Sample Test Cases:
+Input 1:
+6
+Output 1:
+1 2 3 6
+
+Input 2:
+10
+Output 2:
+1 2 5 10
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    int num, i;
+
+    // Taking input
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Factors of %d are: ", num);
+
+    // Checking all numbers from 1 to num
+    for (i = 1; i <= num; i++) {
+        if (num % i == 0) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
