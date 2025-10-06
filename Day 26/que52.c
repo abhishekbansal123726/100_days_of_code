afae6bfc11ab6b099055f14d6687aa29a1461e30
abhishekbansal123726 <abhishekbansal2806@gmail.com>
2025-10-07 00:57:47 +0530

    Update que52.c

---
+++ Day 26/que52.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+
+    // First single star
+    printf("*\n\n");
+
+    // Two stars
+    printf("*\n");
+    printf("*\n\n");
+
+    // Five stars
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*\n\n");
+
+    // Three stars
+    printf("*\n");
+    printf("*\n");
+    printf("*\n\n");
+
+    // Last single star
+    printf("*\n");
+
+    return 0;
+}
+
