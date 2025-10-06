608fba9745fba761ff26988907ef8185b2b20f08
abhishekbansal123726 <abhishekbansal2806@gmail.com>
2025-10-07 00:54:23 +0530

    Update and rename que2.c to que48.c

---
+++ Day 24/que48.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    // Outer loop for rows
+    for (i = 1; i <= 5; i++) {
+        // Inner loop for numbers in each row
+        for (j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+        printf("\n"); // Move to the next line
+    }
+
+    return 0;
+}
+
